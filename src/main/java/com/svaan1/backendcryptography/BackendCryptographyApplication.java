133d3328ee7f1ad651ef2b45e579a7f3bfc8c277
Paulo Ricardo <plricardo504@gmail.com>
2023-11-30 12:40:12 -0300

    Initial Commit

---
+++ src/main/java/com/svaan1/backendcryptography/BackendCryptographyApplication.java
@@ -0,0 +1,13 @@
+package com.svaan1.backendcryptography;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendCryptographyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendCryptographyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/svaan1/backendcryptography/BackendCryptographyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.svaan1.backendcryptography;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendCryptographyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
