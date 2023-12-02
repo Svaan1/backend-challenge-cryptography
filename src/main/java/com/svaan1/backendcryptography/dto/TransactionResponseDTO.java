5c5b4e91b42fdc585c68f0ac9823c13ae6c8018c
Paulo Ricardo <plricardo504@gmail.com>
2023-12-02 18:21:18 -0300

    feat: Create transaction response DTO

--- src/main/java/com/svaan1/backendcryptography/dto/TransactionResponseDTO.java
+++ src/main/java/com/svaan1/backendcryptography/dto/TransactionResponseDTO.java
@@ -3,9 +3,16 @@ package com.svaan1.backendcryptography.dto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor
 public class TransactionResponseDTO {
+    private Long id;
+    private String userDocument;
+    private String creditCardToken;
+    private Long value;
+
 }
