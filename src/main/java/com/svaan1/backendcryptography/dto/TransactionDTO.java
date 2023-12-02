6a4fe7b8295152cd7fbb476192ee91f4726de7f5
Paulo Ricardo <plricardo504@gmail.com>
2023-12-02 18:21:37 -0300

    feat: Add converter method

--- src/main/java/com/svaan1/backendcryptography/dto/TransactionDTO.java
+++ src/main/java/com/svaan1/backendcryptography/dto/TransactionDTO.java
@@ -1,14 +1,25 @@
 package com.svaan1.backendcryptography.dto;
 
+import com.svaan1.backendcryptography.model.Transaction;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor
 public class TransactionDTO {
     private String userDocument;
     private String creditCardToken;
     private Long value;
+
+    public Transaction toEntity() {
+        return Transaction.builder()
+                .userDocument(userDocument)
+                .creditCardToken(creditCardToken)
+                .value(value)
+                .build();
+    }
 }
